4476fcda2c3f2e976548bbb371c66c811a361758
[AK4_Arfaj] <[ak4_arfaj@gmail.com]>
2024-02-07 15:01:40 +0300

    my message-a

--- classA.java
+++ classA.java
@@ -1,5 +1,6 @@
 public class classA {
     public static void main(String[] args) {
         System.out.println("This is a new thing");
+        System.out.println("This is a new thing, only in the branch");
     }
 }
