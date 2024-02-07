50557798d43ef2adccb9f70a5c1bd4bbbfaac321
[AK4_Arfaj] <[ak4_arfaj@gmail.com]>
2024-02-07 15:20:27 +0300

    from dev2-a

--- classA.java
+++ classA.java
@@ -2,5 +2,6 @@ public class classA {
     public static void main(String[] args) {
         System.out.println("This is a new thing");
         System.out.println("This is a new thing, only in the branch");
+        System.out.println("From second device!");
     }
 }
